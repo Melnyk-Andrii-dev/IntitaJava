b3342824ce39d675afc4e7249f58859883dce206
Melnyk-Andrii-dev <sao2099@gmail.com>
2021-08-02 18:53:37 +0300

    first commit

---
+++ src/duke/choice/Customer.java
@@ -0,0 +1,68 @@
+package duke.choice;
+
+public class Customer {
+
+    private String name;
+    private String size;
+    private Clothing[] items;
+
+    public Customer(String name, int measurement) {
+        this.name = name;
+        setSize(measurement);
+    }
+
+    public double getTotalClothingCost() {
+        double total = 0.0;
+        for (Clothing item : items) {
+            total = total + item.getPrice();
+        }
+        return total;
+    }
+
+    public void addItems(Clothing[] aItems) {
+        items = aItems;
+    }
+
+    public Clothing[] getItems() {
+        return items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setSize(int measurement) {
+
+        switch (measurement) {
+            case 1:
+            case 2:
+            case 3:
+                this.size = "S";
+                break;
+            case 4:
+            case 5:
+            case 6:
+                this.size = "M";
+                break;
+            case 7:
+            case 8:
+            case 9:
+                this.size = "L";
+                break;
+            default:
+                this.size = "XL";
+        }
+    }
+}
---
+++ src/duke/choice/ShopApp.java
@@ -0,0 +1,31 @@
+package duke.choice;
+
+public class ShopApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(Clothing.MINIMUM_PRICE);
+        System.out.println(Clothing.TAX_RATE);
+        Customer c1 = new Customer("Pinky", 3);
+
+        System.out.println("Welcome to our shop " + c1.getName());
+
+        Clothing item1 = new Clothing("Blue Jacket", 20.9,"M" );
+        Clothing item2 = new Clothing("Orange T-Short",10.5, "S" );
+
+        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5, "S"),
+                new Clothing()};
+        items[3].setDescription("Blue T-Short");
+        items[3].setPrice(10.5);
+        items[3].setSize("S");
+
+        c1.addItems(items);
+
+        int measurement = 3;
+
+        for (Clothing item : items) {
+            System.out.println("Item: " + item.getDescription() + ", " + item.getPrice() + ", " + item.getSize());
+        }
+        System.out.println("Total: " + c1.getTotalClothingCost());
+    }
+}
